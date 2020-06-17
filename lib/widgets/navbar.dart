f1bcc5ad5765656d24315a38eb487af0ef5848b7
Abhinav Kumar <abhinav6400625@gmail.com>
2020-06-17 09:36:41 +0530

    navbar

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,144 +1,23 @@
 import 'package:flutter/material.dart';
-import '../utils/responsiveLayout.dart';
-class NavBar extends StatefulWidget {
-  //final Function setStatePage;
+import 'package:flutterapp/Experiment/navBarDesktop.dart';
+import 'package:flutterapp/Experiment/navBarMobile.dart';
 
- // NavBar({Key key, this.setStatePage}) : super(key: key);
-   NavBar({Key key}) : super(key: key);
 
-  @override
-  _NavBarState createState() => _NavBarState();
-}
-
-class _NavBarState extends State<NavBar> {
-  final navLinks = [
-    "Home",
-    "Contact us",
-  ];
-  bool pressMenu = false;
-
-  List<Widget> navItem() {
-    return navLinks.asMap().entries.map((entry) {
-      return Padding(
-        padding: EdgeInsets.only(left: 18),
-        child: InkWell(
-           // onTap: () => {widget.setStatePage(entry.key),pressMenu = false},
-            child: Container(
-                margin: EdgeInsets.all(10),
-                child: Text(entry.value,
-                    style: TextStyle(fontFamily: "Montserrat-Bold")
-                    )
-                 )
-               ),
-      );
-    }).toList();
-  }
+class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if(MediaQuery.of(context).size.width>800)pressMenu=false;
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 45, vertical: 10),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Image.network(
-               'https://i.imgur.com/L8Mpi4R.png',
-            ),
-                ],
-              ),
-              SizedBox(width: 15.0,),
-              if (!ResponsiveLayout.isSmallScreen(context))
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[...navItem()]..add(InkWell(
-                      // onTap: () => {widget.setStatePage(3),
-                      //   setState(() {
-                      //     pressMenu = false;
-                      //   })
-                      // },
-                      child: Container(
-                        margin: EdgeInsets.only(left: 20, right: 20),
-                        width: 120,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [Colors.black, Colors.red],
-                                begin: Alignment.bottomRight,
-                                end: Alignment.topLeft),
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Color(0xFF6078ea).withOpacity(.3),
-                                  offset: Offset(0, 8),
-                                  blurRadius: 8)
-                            ]),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: Center(
-                            child: Text("About",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                    letterSpacing: 1,
-                                    fontFamily: "Montserrat-Bold")
-                            ),
-                          ),
-                        ),
-                      ))),
-                )
-              else
-                InkWell(
-                    onTap: () => {
-                          setState(() {
-                            pressMenu = !pressMenu;
-                          })
-                        },
-                    child:
-                    Image.asset("assets/menu.png", width: 26, height: 26))
-            ],
-          ),
-          (pressMenu) ? Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column( crossAxisAlignment:CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[...navItem()]..add(InkWell(
-                //onTap: () =>{ widget.setStatePage(3),pressMenu=false},
-                child: Container(
-                  margin: EdgeInsets.only(left: 20, right: 20),
-                  width: 120,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Color(0xFFC86DD7), Color(0xFF3023AE)],
-                          begin: Alignment.bottomRight,
-                          end: Alignment.topLeft),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color(0xFF6078ea).withOpacity(.3),
-                            offset: Offset(0, 8),
-                            blurRadius: 8)
-                      ]),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: Center(
-                      child: Text("About",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              letterSpacing: 1,
-                              fontFamily: "Montserrat-Bold")),
-                    ),
-                  ),
-                )
-                )
-                )
-                 ),
-          ): Container(),
-        ],
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return navBarDesktop();
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+          return navBarMobile();
+        } else {
+          return navBarMobile();
+        }
+      },
     );
   }
 }
+
+
