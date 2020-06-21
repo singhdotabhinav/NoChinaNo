3a5573431a01f6e882d288e6a2030a95b98b2527
Abhinav Kumar <abhinav6400625@gmail.com>
2020-06-21 21:08:49 +0530

    washing machine page updated

--- lib/ProductPage/WashingMachinePage/WashingMachineDetail.dart
+++ lib/ProductPage/WashingMachinePage/WashingMachineDetail.dart
@@ -23,9 +23,9 @@ bool c10 = false;
 bool c11 = false;
 
 List<String> categoryList = [
-	'Washing Machine Under 10000',
-	'Washing Machine Under 25000',
-	'Washing Machine Under 30000'
+	'Washing Machine Under  ₹ 10,000',
+	'Washing Machine Under ₹ 20,000',
+	'Washing Machine Under ₹ 30,000'
 ];
 
 
@@ -60,8 +60,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 90,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/LG-Semi-Automatic-Loading-P6510NBAY-Technology/dp/B081J583KB/ref=sr_1_2?dchild=1&keywords=LG+6.5+kg+Semi+Automatic+Top+Loading+Washing+Machine&qid=1592719716&s=kitchen&sr=1-2',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/3doiL9Y',
+          flipKartUrl: 'http://fkrt.it/LUjcV4uuuN',
         ),
         productListDesktop(
           productRank: 2 ,      
@@ -83,8 +83,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 90,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/Whirlpool-Semi-Automatic-Superb-Atom-70S/dp/B07WGD8QQT/ref=as_li_ss_tl?fst=as:off&keywords=washing+machine&qid=1553116778&refinements=p_n_feature_sixteen_browse-bin:2753056031,p_89:Whirlpool&rnid=3837712031&s=kitchen&sr=1-3&th=1&linkCode=sl1&tag=mrwashingmach-21&linkId=d9657307145325cc38043d3a54a1180f&language=en_IN',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/3148PjE',
+          flipKartUrl: 'http://fkrt.it/LS9K34uuuN',
         ),
         productListDesktop(
           productRank: 3 ,      
@@ -106,8 +106,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 80,
           categorysix: 'VFM',
           ratingsix: 60,
-          amazonUrl:'https://www.amazon.in/Samsung-Semi-Automatic-Loading-Washing-WT725QPNDMP/dp/B00LSJY0JE',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/2AYIl8i',
+          flipKartUrl: 'http://fkrt.it/LU9sI4uuuN',
         ),
       ],
       [
@@ -131,8 +131,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 90,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/Bosch-Fully-Automatic-Loading-Washing-WAB16060IN/dp/B01GEAOHJY/ref=as_li_ss_tl?fst=as:off&keywords=washing+machine&qid=1556118163&refinements=p_n_feature_fifteen_browse-bin:2753053031,p_89:Bosch&rnid=3837712031&s=kitchen&sr=1-4&linkCode=sl1&tag=mrwashingmach-21&linkId=7679f07e55f22080721f62cbaa2a9c2f&language=en_IN',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/2YVjIkX',
+          flipKartUrl: 'http://fkrt.it/LUf!H4uuuN',
         ),
         productListDesktop(
           productRank: 2 ,      
@@ -154,16 +154,16 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 90,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/Bosch-Fully-Automatic-Loading-Washing-WAB16060IN/dp/B01GEAOHJY/ref=as_li_ss_tl?fst=as:off&keywords=washing+machine&qid=1556118163&refinements=p_n_feature_fifteen_browse-bin:2753053031,p_89:Bosch&rnid=3837712031&s=kitchen&sr=1-4&linkCode=sl1&tag=mrwashingmach-21&linkId=7679f07e55f22080721f62cbaa2a9c2f&language=en_IN',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/2YkdhZL',
+          flipKartUrl: 'http://fkrt.it/2I73tPNNNN',
         ),
         productListDesktop(
           productRank: 3 ,      
           imageUrl:'assets/Samsung 6 kg Front Loading Washing Machine.png',   
-          productName: 'Samsung 6kg',
+          productName: 'Samsung 6kg Automatic Washing Machine',
           productPrice: '19,990',
-          productBrand: 'Whirlpool',  
-          productCountry: 'USA',
+          productBrand: 'Samsung',  
+          productCountry: 'South Korea',
           productDescription: 'For people who deal with the dirtiest laundry on an almost daily basis, the Agipeller with 3D scrub pads will do wonder for you. The combo helps you knock off the hardest stains and give the whitest whitewash you ever have seen.',   
           categoryone: 'Size',
           ratingone:70 ,
@@ -177,8 +177,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 60,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/Bosch-Fully-Automatic-Loading-Washing-WAB16060IN/dp/B01GEAOHJY/ref=as_li_ss_tl?fst=as:off&keywords=washing+machine&qid=1556118163&refinements=p_n_feature_fifteen_browse-bin:2753053031,p_89:Bosch&rnid=3837712031&s=kitchen&sr=1-4&linkCode=sl1&tag=mrwashingmach-21&linkId=7679f07e55f22080721f62cbaa2a9c2f&language=en_IN',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/2zV8GE0',
+          flipKartUrl: 'http://fkrt.it/2!EaDPNNNN',
         ),
       ],
       [
@@ -202,8 +202,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 90,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/Bosch-Fully-Automatic-Loading-Washing-WAK24268IN/dp/B00OT9D4SS',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/37OeNGx',
+          flipKartUrl: 'http://fkrt.it/2!XdDPNNNN',
         ),
         productListDesktop(
           productRank: 2 ,      
@@ -225,8 +225,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 80,
           categorysix: 'VFM',
           ratingsix: 80,
-          amazonUrl:'https://www.amazon.in/IFB-Fully-Automatic-Senorita-Aqua-SX/dp/B00FZCA51C',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/2YlzOoW',
+          flipKartUrl: 'http://fkrt.it/L!awH4uuuN',
         ),
         productListDesktop(
           productRank: 3 ,      
@@ -248,8 +248,8 @@ class _LargeAppWashingMachineState extends State<LargeAppWashingMachine> {
           ratingfive: 60,
           categorysix: 'VFM',
           ratingsix: 60,
-          amazonUrl:'https://www.amazon.in/LG-Fully-Automatic-Loading-Washing-FH2G6HDNL42/dp/B071DN517K',
-          flipKartUrl: '',
+          amazonUrl:'https://amzn.to/3fIwBWj',
+          flipKartUrl: 'http://fkrt.it/2afXDPNNNN',
         ),
 
       ]
@@ -404,22 +404,217 @@ class _SmallAppWashingMachineState extends State<SmallAppWashingMachine> {
     List<List<Widget>> productList = [
       [
         productListMobile(
-          productRank: 1,
-          imageUrl:'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/z/a/f/iphone-11-pro-max-256-u-mwhm2hn-a-apple-0-original-imafkg2ftc5cze5n.jpeg?q=70',
-          productName: 'iPHONE 11 PRO',
-          productPrice: ' 1,21,000.00',
-          productBrand: 'APPLE Inc.',  
-          productCountry: 'USA',
-          productDescription: 'The iPhone 11 Pro is one of the best mobile phones in India, From a Compay that respects privacy and security of their customers. It is powered with Apple\'s latest and greatest A13 Bionic Chip. On the top u get excelent display. on back it features a 12MP triple Camera Setup.',   
-          ratingone: 100,
-          ratingthree: 100,
-          ratingtwo: 100,
+          productRank: 1  ,      
+         imageUrl:'assets/LG 6.5 Kg 4 Star Semi-Automatic Top Loading Washing Machine.png',   
+          productName: 'LG 6.5Kg Semi-Automatic',
+          productPrice: '28,999',
+          productBrand: 'LG',  
+          productCountry: 'South Korea',
+          productDescription: 'The washing machine is adorned and powered with a 360W motor which products 1350 rpm spin cycle the perfect to dry your laundry the fastest way.',   
+          categoryone: 'Size',
+          ratingone:90 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
           ratingfour: 90,
-          ratingsix: 100,
-          ratingfive: 80,
-          amazonUrl:'https://amzn.to/2ADJlPd',
-          flipKartUrl: 'http://fkrt.it/s49TtfuuuN', 
+          categoryfive: 'Noise',
+          ratingfive: 90,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/3doiL9Y',
+          flipKartUrl: 'http://fkrt.it/LUjcV4uuuN',
         ),
+        productListMobile(
+          productRank: 2 ,      
+         imageUrl:'assets/Whirlpool 7 Kg 5 Star Semi-Automatic Top Loading Washing Machine.png',   
+          productName: 'Whirlpool 7Kg',
+          productPrice: '8,999',
+          productBrand: 'Whirlpool',  
+          productCountry: 'USA',
+          productDescription: 'Whirlpool 7KG Washing machine is something really an exception in the market. The aesthetics, power performance, RPM and after-sale services all are just perfect.',   
+          categoryone: 'Size',
+          ratingone:80 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 70,
+          categoryfive: 'Noise',
+          ratingfive: 90,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/3148PjE',
+          flipKartUrl: 'http://fkrt.it/LS9K34uuuN',
+        ),
+        productListMobile(
+          productRank: 3 ,      
+         imageUrl:'assets/Samsung 7.2 kg Semi-Automatic Top Loading Washing Machine.png',   
+          productName: 'Samsung 7.2kg ',
+          productPrice: '10,100',
+          productBrand: 'Samsung',  
+          productCountry: 'South Korea',
+          productDescription: 'The Samsung washer looks sober and is equipped with state of the art technology. Here you see every problem is nailed down following an out of the box method.',   
+          categoryone: 'Size',
+          ratingone:80 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 80,
+          categorysix: 'VFM',
+          ratingsix: 60,
+          amazonUrl:'https://amzn.to/2AYIl8i',
+          flipKartUrl: 'http://fkrt.it/LU9sI4uuuN',
+        ),
+      ],
+      [
+        productListMobile(
+          productRank: 1 ,      
+          imageUrl:'assets/Bosch 6 kg Fully-Automatic Front Loading Washing Machine.png',   
+          productName: 'Bosch 6kg ',
+          productPrice: '21,650',
+          productBrand: 'Bosch',  
+          productCountry: 'Germany',
+          productDescription: 'Bosch 6 kg Fully-Automatic Front Loading Washing Machine',   
+          categoryone: 'Size',
+          ratingone:90 ,
+          categorytwo:'Durability' ,
+          ratingtwo:90,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 90,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/2YVjIkX',
+          flipKartUrl: 'http://fkrt.it/LUf!H4uuuN',
+        ),
+        productListMobile(
+          productRank: 2 ,      
+          imageUrl:'assets/Whirlpool 8 Kg Fully-Automatic Top Loading Washing Machine with In-Built Heater.png',   
+          productName: 'Whirlpool 8Kg ',
+          productPrice: '19,990',
+          productBrand: 'Whirlpool',  
+          productCountry: 'USA',
+          productDescription: 'For people who deal with the dirtiest laundry on an almost daily basis, the Agipeller with 3D scrub pads will do wonder for you. The combo helps you knock off the hardest stains and give the whitest whitewash you ever have seen.',   
+          categoryone: 'Size',
+          ratingone:90 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 70,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 90,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/2YkdhZL',
+          flipKartUrl: 'http://fkrt.it/2I73tPNNNN',
+        ),
+        productListMobile(
+          productRank: 3 ,      
+          imageUrl:'assets/Samsung 6 kg Front Loading Washing Machine.png',   
+          productName: 'Samsung 6kg Automatic',
+          productPrice: '19,990',
+          productBrand: 'Samsung',  
+          productCountry: 'South Korea',
+          productDescription: 'For people who deal with the dirtiest laundry on an almost daily basis, the Agipeller with 3D scrub pads will do wonder for you. The combo helps you knock off the hardest stains and give the whitest whitewash you ever have seen.',   
+          categoryone: 'Size',
+          ratingone:70 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 60,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/2zV8GE0',
+          flipKartUrl: 'http://fkrt.it/2!EaDPNNNN',
+        ),
+      ],
+      [
+        productListMobile(
+          productRank: 1 ,      
+          imageUrl:'assets/Bosch 7 kg Fully-Automatic Front Loading Washing Machine .png',   
+          productName: 'Bosch 7kg Fully-Automatic',
+          productPrice: '33,469',
+          productBrand: 'Bosch',  
+          productCountry: 'Germany',
+          productDescription: 'VarioDrum, ActiveWater, VoltCheck, Foam detection system, Unbalanced load detection, Multiple water protection, Allergy Plus, ECARF, Eco Perfect, Active Water and Water Plus, Auto Restart,Fabric Softener Dispenser',   
+          categoryone: 'Size',
+          ratingone:90 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 90,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/37OeNGx',
+          flipKartUrl: 'http://fkrt.it/2!XdDPNNNN',
+        ),
+        productListMobile(
+          productRank: 2 ,      
+          imageUrl:'assets/IFB 6.5 kg Fully-Automatic Front Loading Washing Machine.png',   
+          productName: 'IFB 6.5kg Fully-Automatic',
+          productPrice: '29,490',
+          productBrand: 'IFB',  
+          productCountry: 'INDIA',
+          productDescription: 'Self Diagnosis, Auto Imbalance Sensing & Control, Foam Control System, Protective Rat Mesh',   
+          categoryone: 'Size',
+          ratingone:80 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 80,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 80,
+          categorysix: 'VFM',
+          ratingsix: 80,
+          amazonUrl:'https://amzn.to/2YlzOoW',
+          flipKartUrl: 'http://fkrt.it/L!awH4uuuN',
+        ),
+        productListMobile(
+          productRank: 3 ,      
+          imageUrl:'assets/LG 7 kg Inverter Fully-Automatic Front Loading Washing Machine.png',   
+          productName: 'LG 7Kg Fully Automatic',
+          productPrice: '29,490',
+          productBrand: 'LG', 
+          productCountry: 'South Korea',
+          productDescription: 'Fully-automatic front-loading washing machine. Capacity 7 kg: Suitable for families with 3 to 4 members. Door Opening Angle 170 Deg',   
+          categoryone: 'Size',
+          ratingone:80 ,
+          categorytwo:'Durability' ,
+          ratingtwo:80,
+          categorythree: 'Features',
+          ratingthree: 60,
+          categoryfour: 'Design',
+          ratingfour: 90,
+          categoryfive: 'Noise',
+          ratingfive: 60,
+          categorysix: 'VFM',
+          ratingsix: 60,
+          amazonUrl:'https://amzn.to/3fIwBWj',
+          flipKartUrl: 'http://fkrt.it/2afXDPNNNN',
+        ),
+
       ]
     ];
     return SingleChildScrollView(
@@ -524,52 +719,8 @@ class _SmallAppWashingMachineState extends State<SmallAppWashingMachine> {
                           title: categoryList[2],
                           color: c2 ? Colors.black12 : Colors.white),
                     ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 3;
-                          categoryListIndex = 3;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = true;
-                          c4 = false;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[3],
-                          color: c3 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 4;
-                          categoryListIndex = 4;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = true;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[4],
-                          color: c4 ? Colors.black12 : Colors.white),
-                    ),
+                   
+                    
                   ],
                 ),
               ),
@@ -586,7 +737,7 @@ class _SmallAppWashingMachineState extends State<SmallAppWashingMachine> {
                     autoPlay: false,
                     enlargeCenterPage: true,
                   ),
-                  items: [1, 2, 3, 4, 5].map((i) {
+                  items: [1, 2, 3,].map((i) {
                     return Builder(
                       builder: (BuildContext context) {
                         return productList[productListX][i - 1];
