ea3e746d9c6dd5fcc5c08c58e6c7f1b557da2154
Abhinav Kumar <abhinav6400625@gmail.com>
2020-06-21 20:38:29 +0530

    mobile view for fridge page

--- lib/ProductPage/RefrigiratorPage/RefrigiratorPageDetail.dart
+++ lib/ProductPage/RefrigiratorPage/RefrigiratorPageDetail.dart
@@ -403,24 +403,218 @@ class _SmallAppRefrigeratorState extends State<SmallAppRefrigerator> {
   @override
   Widget build(BuildContext context) {
     List<List<Widget>> productList = [
+            [
+        productListMobile(
+          productRank:  1,       
+          imageUrl:'assets/LG 45 L Direct-cool Single Door Refrigerator.png',   
+          productName: 'LG 45 L Direct-cool ',
+          productPrice: '7, 990',
+          productBrand: 'LG',  
+          productCountry: 'South Korea',
+          productDescription: 'Inside the fridge, you can get enough space to stock items like bottles, cans, fruits, and snacks, etc with extra long freshness. This small refrigerator is also equipped with powerful cooling technology that gives you the best cooling experience.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 80,
+          categorythree: 'Design',
+          ratingthree: 80, 
+          categoryfour: 'Weight',
+          ratingfour: 80,
+          categoryfive: 'VLM',
+          ratingfive: 80,
+          categorysix: 'Reviews',
+          ratingsix: 80,   
+          amazonUrl:'https://www.amazon.in/LG-Direct-cool-Single-Refrigerator-GL-051SSW/dp/B00QA1OY0A/ref=sr_1_4?dchild=1&keywords=LG+GL-051SSW+Direct-cool+Single+Door+Mini+Refrigerator&qid=1592710972&s=kitchen&sr=1-4',
+          flipKartUrl: '', 
+        ),
+        productListMobile(
+          productRank:  2,       
+          imageUrl:'assets/Mitashi 52 L 2 Star ( 2019 ) Direct-Cool Single Door Refrigerator.png',   
+          productName: 'Mitashi 52L 2 Star ',
+          productPrice: '7, 490',
+          productBrand: 'Mitashi',  
+          productCountry: 'India',
+          productDescription: 'This mini refrigerator is featured with direct cooling system ensuring faster and optimum cooling and superb thermodynamic performance at low cost.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 70,
+          categorythree: 'Design',
+          ratingthree: 80, 
+          categoryfour: 'Weight',
+          ratingfour: 80,
+          categoryfive: 'VLM',
+          ratingfive: 80,
+          categorysix: 'Reviews',
+          ratingsix: 80,   
+          amazonUrl:'https://www.amazon.in/Mitashi-Direct-Cool-Single-Refrigerator-MSD052RF200/dp/B07RYLK3LL/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=lko01-21&linkId=82c25fd8cc78748ef77604dd8ca563ec&language=en_IN',
+          flipKartUrl: '', 
+        ),
+        productListMobile(
+          productRank:  3,       
+          imageUrl:'assets/Whirlpool 46 L 3 Star.png',   
+          productName: 'Whirlpool 46L 3 Star',
+          productPrice: '8, 490',
+          productBrand: 'Whirlpool',  
+          productCountry: 'USA',
+          productDescription: 'There is a separate freezer section to store ice cream and other frozen delights. With seven adjustable cooling modes, you can choose the perfect temperature for all your cooling needs. ',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 70,
+          categorythree: 'Design',
+          ratingthree: 70, 
+          categoryfour: 'Weight',
+          ratingfour: 70,
+          categoryfive: 'VLM',
+          ratingfive: 70,
+          categorysix: 'Reviews',
+          ratingsix: 70,   
+          amazonUrl:'https://www.amazon.in/Whirlpool-Refrigerator-65-W-ATOM-PRM-3S-Steel/dp/B07QP1J91D/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=lko01-21&linkId=49f6299a1197352b116b2371616a4b45&language=en_IN',
+          flipKartUrl: '', 
+        ),
+      ],
       [
         productListMobile(
-          productRank: 1,
-          imageUrl:'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/z/a/f/iphone-11-pro-max-256-u-mwhm2hn-a-apple-0-original-imafkg2ftc5cze5n.jpeg?q=70',
-          productName: 'iPHONE 11 PRO',
-          productPrice: ' 1,21,000.00',
-          productBrand: 'APPLE Inc.',  
-          productCountry: 'USA',
-          productDescription: 'The iPhone 11 Pro is one of the best mobile phones in India, From a Compay that respects privacy and security of their customers. It is powered with Apple\'s latest and greatest A13 Bionic Chip. On the top u get excelent display. on back it features a 12MP triple Camera Setup.',   
-          ratingone: 100,
-          ratingthree: 100,
-          ratingtwo: 100,
-          ratingfour: 90,
-          ratingsix: 100,
+          productRank:  1,       
+          imageUrl:'assets/LG 190 L 4 Star Inverter Direct-Cool Single Door Refrigerator.png',   
+          productName: 'LG 190 L 4 Star',
+          productPrice: '15, 190',
+          productBrand: 'LG',  
+          productCountry: 'South Korea',
+          productDescription: 'This LG fridge under 20000 works without a stabiliser and has a smart inverter compressor.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 80,
+          categorythree: 'Design',
+          ratingthree: 80, 
+          categoryfour: 'Weight',
+          ratingfour: 80,
+          categoryfive: 'VLM',
           ratingfive: 80,
-          amazonUrl:'https://amzn.to/2ADJlPd',
-          flipKartUrl: 'http://fkrt.it/s49TtfuuuN', 
+          categorysix: 'Reviews',
+          ratingsix: 80,   
+          amazonUrl:'https://www.amazon.in/LG-Refrigerator-GL-B201ASPY-ASPZEBN-Plumeria-Compressor/dp/B079QP11T3/ref=sr_1_4?dchild=1&keywords=LG+190L+Single+Door+Refrigerator&qid=1592712328&s=kitchen&sr=1-4',
+          flipKartUrl: '', 
+        ),productListMobile(
+          productRank:  2,       
+          imageUrl:'assets/Whirlpool 215 L 3 Star Inverter Direct-Cool Single Door Refrigerator with Moisture Lock-In Technology.png',   
+          productName: 'Whirlpool 46L 3 Star',
+          productPrice: '15, 490',
+          productBrand: 'Whirlpool',  
+          productCountry: 'USA',
+          productDescription: 'A refrigerator below 20000, the Whirlpool IceMagic Fresh 215L houses an inverter compressor and is known for its efficiency in ice making.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 70,
+          categorythree: 'Design',
+          ratingthree: 70, 
+          categoryfour: 'Weight',
+          ratingfour: 70,
+          categoryfive: 'VLM',
+          ratingfive: 70,
+          categorysix: 'Reviews',
+          ratingsix: 70,   
+          amazonUrl:'https://www.amazon.in/Whirlpool-Refrigerator-230-PRM-INV/dp/B083KSWRFW/ref=sr_1_4?dchild=1&keywords=Whirlpool+215L+Single+Door+Refrigerator&qid=1592712564&s=kitchen&sr=1-4',
+          flipKartUrl: '', 
+        ),productListMobile(
+          productRank:  3,       
+          imageUrl:'assets/Godrej 236 L 2 Star Inverter Frost-Free Double Door Refrigerator.png',   
+          productName: 'Godrej 236L 2 Star',
+          productPrice: '18, 490',
+          productBrand: 'Godrej',  
+          productCountry: 'India',
+          productDescription: 'In the running for the best double door refrigerator under 20000, this Godrej fridge offers a child lock option and ample freezer space.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 80,
+          categorythree: 'Design',
+          ratingthree: 90, 
+          categoryfour: 'Weight',
+          ratingfour: 60,
+          categoryfive: 'VLM',
+          ratingfive: 70,
+          categorysix: 'Reviews',
+          ratingsix: 90,   
+          amazonUrl:'https://www.amazon.in/Godrej-Frost-Free-Refrigerator-EON-236B/dp/B083SQK6GH/ref=sr_1_5?dchild=1&keywords=Godrej+236L+Double+Door+Refrigerator&qid=1592712762&s=kitchen&sr=1-5',
+          flipKartUrl: '', 
         ),
+      ],
+      [
+        productListMobile(
+          productRank:  1,       
+          imageUrl:'assets/LG 260 L 3 Star Frost Free Double Door Refrigerator.png',   
+          productName: 'LG 260L 3 Star',
+          productPrice: '24, 490',
+          productBrand: 'LG',  
+          productCountry: 'South Korea',
+          productDescription: 'The refrigerator model received a 4-star rating that allows the user to save considerable power while being the best fridge under 30000',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 90,
+          categorythree: 'Design',
+          ratingthree: 90, 
+          categoryfour: 'Weight',
+          ratingfour: 90,
+          categoryfive: 'VLM',
+          ratingfive:90,
+          categorysix: 'Reviews',
+          ratingsix: 90,   
+          amazonUrl:'https://www.amazon.in/LG-Refrigerator-GL-I292RPZL-APZZEBN-Inverter-Compressor/dp/B01DM8HR1Q/ref=as_li_ss_tl?s=kitchen&ie=UTF8&qid=1577536412&sr=1-3&linkCode=sl1&tag=refrigeratorg-21&linkId=69e7541475fcb688dff800816fd021ff&language=en_IN',
+          flipKartUrl: '', 
+        ),
+        productListMobile(
+          productRank:2,       
+          imageUrl:'assets/Samsung 244L 3 Star Inverter Frost Free Double Door Refrigerator.png',   
+          productName: 'Samsung 244L 3 Star',
+          productPrice: '26, 180',
+          productBrand: 'Samsung',  
+          productCountry: 'South Korea',
+          productDescription: 'The refrigerator contains a control panel to adjust the temperature of the freezer as per the requirement. You can run it at normal mode, vacation mode, mini-fridge mode or convert the freezer to the fridge too.',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 80,
+          categorythree: 'Design',
+          ratingthree: 80, 
+          categoryfour: 'Weight',
+          ratingfour: 80,
+          categoryfive: 'VLM',
+          ratingfive: 80,
+          categorysix: 'Reviews',
+          ratingsix: 90,   
+          amazonUrl:'https://www.amazon.in/Samsung-Refrigerator-RT28M3424S8-HL-Compressor/dp/B06XCB7LPD/ref=as_li_ss_tl?s=kitchen&ie=UTF8&qid=1577536412&sr=1-2&linkCode=sl1&tag=refrigeratorg-21&linkId=9405e1cdabb683f0c0fff561f5295012&language=en_IN',
+          flipKartUrl: '', 
+        ),
+        productListMobile(
+          productRank:3,       
+          imageUrl:'assets/Whirlpool 265 L 3 Star Inverter Frost-Free Double Door Refrigerator.png',   
+          productName: 'Whirlpool 265L 3 Star',
+          productPrice: '23, 980',
+          productBrand: 'Whirlpool',  
+          productCountry: 'USA',
+          productDescription: 'The refrigerator consumes about 260 units of electricity yearly and can also run stabilizer free between 130-300 Volt fluctuations. ',   
+          categoryone: 'Durabilty',
+          ratingone: 90,
+          categorytwo:'Size' ,
+          ratingtwo: 60,
+          categorythree: 'Design',
+          ratingthree: 60, 
+          categoryfour: 'Weight',
+          ratingfour: 60,
+          categoryfive: 'VLM',
+          ratingfive: 90,
+          categorysix: 'Reviews',
+          ratingsix: 90,   
+          amazonUrl:'https://www.amazon.in/Whirlpool-INV-CNV-278-ELT/dp/B07QV1LNYJ/ref=as_li_ss_tl?s=kitchen&ie=UTF8&qid=1577536412&sr=1-15&linkCode=sl1&tag=refrigeratorg-21&linkId=a252c72cdb144bca2749fb6838bb26ad&language=en_IN',
+          flipKartUrl: '', 
+        ),
+
+
       ]
     ];
     return SingleChildScrollView(
@@ -525,167 +719,6 @@ class _SmallAppRefrigeratorState extends State<SmallAppRefrigerator> {
                           title: categoryList[2],
                           color: c2 ? Colors.black12 : Colors.white),
                     ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 3;
-                          categoryListIndex = 3;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = true;
-                          c4 = false;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[3],
-                          color: c3 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 4;
-                          categoryListIndex = 4;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = true;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[4],
-                          color: c4 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 5;
-                          categoryListIndex = 5;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = false;
-                          c5 = true;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[5],
-                          color: c5 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 6;
-                          categoryListIndex = 6;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = false;
-                          c5 = false;
-                          c6 = true;
-                          c7 = false;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[6],
-                          color: c6 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 7;
-                          categoryListIndex = 7;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = false;
-                          c5 = false;
-                          c6 = false;
-                          c7 = true;
-                          c8 = false;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[7],
-                          color: c7 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 8;
-                          categoryListIndex = 8;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = false;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = true;
-                          c9 = false;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[8],
-                          color: c8 ? Colors.black12 : Colors.white),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          productListX = 9;
-                          categoryListIndex = 9;
-                          c0 = false;
-                          c1 = false;
-                          c2 = false;
-                          c3 = false;
-                          c4 = false;
-                          c5 = false;
-                          c6 = false;
-                          c7 = false;
-                          c8 = false;
-                          c9 = true;
-                          c10 = false;
-                          c11 = false;
-                        });
-                      },
-                      child: MenuCard(
-                          title: categoryList[9],
-                          color: c9 ? Colors.black12 : Colors.white),
-                    ),
 
                     //  Image.network("assets/myapp/unnamed.png"),
                   ],
@@ -704,7 +737,7 @@ class _SmallAppRefrigeratorState extends State<SmallAppRefrigerator> {
                     autoPlay: false,
                     enlargeCenterPage: true,
                   ),
-                  items: [1, 2, 3, 4, 5].map((i) {
+                  items: [1, 2, 3,].map((i) {
                     return Builder(
                       builder: (BuildContext context) {
                         return productList[productListX][i - 1];
